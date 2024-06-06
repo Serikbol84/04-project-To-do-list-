e17b6b1937fdf54aaeed4b8b724d976a87454596
Serikbol <sh.serikbol@gmail.com>
2024-06-06 11:36:38 +0500

    starting

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="cantainer">
+                <div className="title">
+                    <h1>Simple To Do List</h1>
+                    <p>Today is awesome day. The weather is awesome, you are awesome too!</p>
+                </div>
+            </div>
+        </header>
+
+    );
+}
+ 
+export default Header;
