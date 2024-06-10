78aee7e22ef5a73a3db596c82bc8cd2861544ef3
Serikbol <sh.serikbol@gmail.com>
2024-06-10 21:51:42 +0500

    doing dinamyc

--- src/Components/Main/Pager/Pager.jsx
+++ src/Components/Main/Pager/Pager.jsx
@@ -6,124 +6,184 @@ import { useState, useEffect } from 'react';
 
 const Pager = () => {
     const [activeTab, setActiveTab] = useState ("TODO");
-    const [isMenuVisible, setMenuVisible] = useState (false);
-    const [toDoText, setToDoText] = useState ('');
+    const [isMenuVisible, setIsMenuVisible] = useState (false);
+    const [toDoText, setToDoText] = useState('')
+    const [toDos, setToDos] = useState ([
+        {   
+            id: 123,
+            name:'TODO',
+            title: "To do",
+            isTrash: true,
+            moveBack: false,
+            children: [
+            {
+                id: 1,
+                checked: false,
+                item: 'Write Essay'
+            },
+            {
+                id: 2,
+                checked: false,
+                item: 'One Hour CSS Course Online',
+            },
+            {
+                id: 3,
+                checked: false,
+                item: 'Buy One Way Tickets to San Fransico',
+            },
+            {
+                id: 4,
+                checked: false,
+                item: 'Go to Gym',
+            },
+            {
+                id: 5,
+                checked: false,
+                item: 'Buy Groceries',
+            }
+           ] 
+        },
+        {
+            id: 456,
+            name:'DONE',
+            title: "Done",
+            isTrash: false,
+            moveBack: false,
+            children: [
+                {
+                    id: 1,
+                    checked: false,
+                    item: 'Write Essay'
+                },
+                {
+                    id: 2,
+                    checked: false,
+                    item: 'One Hour CSS Course Online',
+                },
+                {
+                    id: 3,
+                    checked: false,
+                    item: 'Buy One Way Tickets to San Fransico',
+                },
+                {
+                    id: 4,
+                    checked: false,
+                    item: 'Go to Gym',
+                },
+                {
+                    id: 5,
+                    checked: false,
+                    item: 'Buy Groceries',
+                }
+            ] 
+        },
+        {
+            id: 789,
+            name:'TRASH',
+            title: "Trash",
+            isTrash: false,
+            moveBack: true,
+            children: [
+                {
+                    id: 1,
+                    checked: false,
+                    item: 'Write Essay'
+                },
+                {
+                    id: 2,
+                    checked: false,
+                    item: 'Write Essay'
+                },
+                {
+                    id: 3,
+                    checked: false,
+                    item: 'Write Essay'
+                }
+            ] 
+         }
+    ]);
     
-    useEffect(() => {
-        const textFromStorage = localStorage.getItem('inputTextArea')
-        if (textFromStorage) {
-            setToDoText(textFromStorage)
-        } 
-    }, [])
-
+    
+    console.log(activeTab)
     const renderContent = () => {
-        switch (activeTab) {
-            case 'TODO':
-                return (
-                    <section className="toDo"> 
-                        <div className="container">
-                            <div className="toDoTile">
-                                <h1>To Do</h1>
-                            </div>
-                            <div className="divider">
-                                <span className='divider'></span>
-                            </div>
-                        </div>
 
-                        <section className="itemList">
-                            <div className="container">
-                                <div className="wrapper">
-                                    <div className="actions">
-                                        <div className="dots">
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
-                                        </div>
-                                        <input type="checkbox" className="checkbox" />
-                                        <input type="text" className='inputRead'/>
-                                    </div>
-                                </div>
-                                
-                                <div className="moveToTrash">
-                                    <img src={Trash} alt="" />
-                                    <p>Move to Trash</p>
-                                </div>
-                            </div>
-                        </section>
-                    </section>
-                )
-            case 'DONE':
-                return (
-                    <section className="done"> 
-                        <div className="container">
-                            <div className="doneTile">
-                                <h1>Done</h1>
-                            </div>
-                            <div className="divider">
-                                <span className='divider'></span>
-                            </div>
-                        </div>
+        return toDos.map(toDo => {
+            if (toDo.name === activeTab) {
 
-                        <section className="itemList">
-                            <div className="container">
-                                <div className="wrapper">
-                                    <div className="actions">
-                                        <div className="dots">
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
-                                        </div>
-                                        <input type="checkbox" className="checkbox" />
-                                        <input type="text" value='' className='inputRead' />
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </section>
-                )
-            case 'TRASH':
                 return (
-                    <section className="trash"> 
+                    <section className='trash'>
                         <div className="container">
-                            <div className="trashTile">
-                                <h1>Trash</h1>
+                            <div className="title">
+                                <h1>{toDo.title}</h1>
                             </div>
                             <div className="divider">
                                 <span className='divider'></span>
                             </div>
-                        </div>
-                        <section className="itemList">
-                            <div className="container">
-                                <div className="wrapper">
-                                    <div className="actions">
-                                        <div className="dots">
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
-                                            <span className='dot'></span>
+                        </div> 
+
+                        {
+                            toDo.children.map(child => {
+                                return (
+                                    <section className="itemList" key={child.id}>
+                                        <div className="container">
+                                            <div className="wrapper">
+                                                <div className="actions">
+                                                    <div className="dots">
+                                                        <span className='dot'></span>
+                                                        <span className='dot'></span>
+                                                        <span className='dot'></span>
+                                                    </div>
+                                                    <input type="checkbox" checked={false} className="checkbox" />
+                                                    <input type="text" value={child.item} className='inputRead' />
+                                                </div>
+                                            </div>
                                         </div>
-                                        <input type="checkbox" className="checkbox" />
-                                        <input type="text" value='' className='inputRead' />
-                                    </div>
-                                </div>
-                                
+                                    </section>
+                                )
+                            })
+                        }
+
+                        {
+                            (toDo.isTrash === true) && (
                                 <div className="trashMenu">
-                                    <div className="delete">
+                                    <button className="delete">
                                         <img src={Trash} alt="" />
-                                        <p>Delete Forever</p>
-                                    </div>
-                                    <div className="moveBack">
+                                        <p>Move to trash</p>
+                                    </button>
+                                </div>
+                            )
+                        }
+
+                        {
+                            (toDo.moveBack === true) && (
+                                <div className='trashMenu'>
+                                    <button className="delete">
+                                    <img src={Trash} alt="" />
+                                    <p>Delete Forever</p>
+                                    </button>
+                                    <button className="moveBack">
                                         <img src={Library} alt="" />
                                         <p>Move Back To To Do</p>
-                                    </div>
+                                    </button>
                                 </div>
-                            </div>
-                        </section>
+                            )
+                        }
+
                     </section>
                 )
-            default:
-                return null;
-        }
+
+            }
+           
+        })
+
     };
+
+    // useEffect(() => {
+    //     const textFromStorage = localStorage.getItem('inputTextArea')
+    //     if (textFromStorage) {
+    //         setToDoText(textFromStorage)
+    //     } 
+    // }, [])
 
     function handleTextChange(e) {
         setToDoText(e.target.value)
@@ -138,39 +198,34 @@ const Pager = () => {
         <section className="pager">
             <div className="container">
                 <div className="pagerLine">
+                    {
+                        toDos.map(toDo  => {
+                            return (
+                                <button 
+                                    className="btn"
+                                    style={{backgroundColor: activeTab === toDo.name && "rgba(8, 30, 52, 0.42)",
+                                    color: activeTab === toDo.name && "white"}}
+                                    onClick={() => setActiveTab (toDo.name)}
+                                >
+                                    <span>{toDo.name}</span>
+                                </button>
+                            )
+                        })
 
-                    <button className="btn"
-                        style={{backgroundColor: activeTab === "TODO" && "rgba(8, 30, 52, 0.42)",
-                                color: activeTab === "TODO" && "white"}}
-                                onClick={() => setActiveTab ("TODO")}>
-                        <span>To Do</span>
-                    </button>
+                    }
 
-                    <button className="btn" 
-                        style={{backgroundColor: activeTab === "DONE" && "rgba(8, 30, 52, 0.42)",
-                                color: activeTab === "DONE" && "white"}}
-                                onClick={() => setActiveTab ("DONE")}>
-                        <span>Done</span>
-                    </button>
-
-                    <button className="btn" 
-                        style={{backgroundColor: activeTab === "TRASH" && "rgba(8, 30, 52, 0.42)",
-                                color: activeTab === "TRASH" && "white"}}
-                                onClick={() => setActiveTab ("TRASH")}>
-                            <span>Trash</span>
-                    </button>
 
                 </div>
 
                 <div className="addToDos">
-                    <button className='plus' onClick={() => setMenuVisible (!isMenuVisible)}></button>
+                    <button className='plus' onClick={() => setIsMenuVisible (!isMenuVisible)}></button>
                 </div>
 
                 {isMenuVisible && (
                 <div className="inputTask">
                     <div className="inputTaskWrapper">
                         <h1>Add New To Do</h1>
-                        <textarea type="text" className='tasks' placeholder="Your text" value={toDoText} onChange={handleTextChange}/>
+                        <textarea type="text" className='tasks' placeholder="Your text" value={toDoText} />
                         <button className="add" onClick={add}>
                             <span>Add</span>
                         </button>
