220bf86c5145b01b0f6d2cbe781225dfa50512ee
Serikbol <sh.serikbol@gmail.com>
2024-06-13 16:12:24 +0500

    Almost finish

--- src/Components/Main/Pager/Pager.jsx
+++ src/Components/Main/Pager/Pager.jsx
@@ -11,31 +11,45 @@ const Pager = () => {
     const [isMenuVisible, setMenuVisible] = useState (false);
     const [toDoText, setToDoText] = useState ('');
     const [items, setItems] = useState ([
-        {
-            id: 1,
-            checked: false,
-            item: 'sdcasdas',
-            onlyTrash: false,
-            toDoDone: true
-        },
-        {
-            id: 2,
-            checked: false,
-            item: 'sdcasdas',
-            onlyTrash: false,
-            toDoDone: false
-        },
-        {
-            id: 3,
-            checked: false,
-            item: 'sdcasdas',
-            onlyTrash: false,
-            toDoDone: true
-        }
+        // {
+        //     id: 1,
+        //     checked: false,
+        //     item: 'Write Essay',
+        //     onlyTrash: false,
+        //     toDoDone: true
+        // },
+        // {
+        //     id: 2,
+        //     checked: false,
+        //     item: 'One Hour CSS Course Online',
+        //     onlyTrash: false,
+        //     toDoDone: true
+        // },
+        // {
+        //     id: 3,
+        //     checked: false,
+        //     item: 'Buy One Way Tickets to San Fransico',
+        //     onlyTrash: false,
+        //     toDoDone: true
+        // },
+        // {
+        //     id: 4,
+        //     checked: false,
+        //     item: 'Go to Gym',
+        //     onlyTrash: false,
+        //     toDoDone: true
+        // },
+        // {
+        //     id: 5,
+        //     checked: false,
+        //     item: 'Buy Groceries',
+        //     onlyTrash: false,
+        //     toDoDone: true
+        // }
     ])
     console.log(items);
-    const filterToDos = items.filter(item => item.toDoDone === true)
-    const filterDones = items.filter(item => item.toDoDone === true && item.checked === true)
+    const filterToDos = items.filter(item => item.toDoDone === true )
+    const filterDones = items.filter(item => item.toDoDone === true && item.checked === true )
     const filterTrashes = items.filter(item => item.onlyTrash === true)
     
     const renderContent = () => {
@@ -53,26 +67,32 @@ const Pager = () => {
                         </div>
 
                         {
-                            filterToDos.map(filterToDo => {
-                                return (
-                                    <section className="itemList" key={filterToDo.id}>
-                                        <div className="container">
-                                            <div className="actions">
-                                                <div className="dots">
-                                                    <span className='dot'></span>
-                                                    <span className='dot'></span>
-                                                    <span className='dot'></span>
-                                                </div>
-                                                <input type="checkbox" className="checkbox" />
-                                                <input type="text" value={filterToDo.item} className='inputRead' />
-                                            </div>  
-                                        </div>
-                                    </section>
-                                )
-                            })
+                            filterToDos.length ? (
+                                filterToDos.map(filterToDo => {
+                                    return (
+                                        <section className="itemList" key={filterToDo.id}>
+                                            <div className="container">
+                                                <div className="actions">
+                                                    <div className="dots">
+                                                        <span className='dot'></span>
+                                                        <span className='dot'></span>
+                                                        <span className='dot'></span>
+                                                    </div>
+                                                    <input type="checkbox" checked={filterToDo.checked} className="checkbox" onChange={ () => handleCheck(filterToDo.id) }/>
+                                                    <input type="text" value={filterToDo.item} className={`inputRead ${filterToDo.checked && 'textThrought'}`} />
+                                                </div>  
+                                            </div>
+                                        </section>
+                                    )
+                                })
+                            ) : 
+                            (
+                                <p style={{ fontSize: "18px", color: "Gray" }}>
+                                Your list is empty!</p>
+                            )
                         }
                         
-                        <button className="moveToTrash">
+                        <button className="moveToTrash" onClick={moveToTrash}>
                             <img src={Trash} alt="" />
                             <p>Move to Trash</p>
                         </button>
@@ -102,8 +122,8 @@ const Pager = () => {
                                                         <span className='dot'></span>
                                                         <span className='dot'></span>
                                                     </div>
-                                                    <input type="checkbox" className="checkbox" />
-                                                    <input type="text" value={filterDone.item} className='inputRead' />
+                                                    <input type="checkbox" className="checkbox" checked={filterDone.checked} onChange={ () => handleCheck(filterDone.id) }/>
+                                                    <input type="text" value={filterDone.item} className={`inputRead ${filterDone.checked && 'textThrought'}`} />
                                                 </div>
                                             </div>
                                         </div>
@@ -139,8 +159,8 @@ const Pager = () => {
                                                         <span className='dot'></span>
                                                         <span className='dot'></span>
                                                     </div>
-                                                    <input type="checkbox" className="checkbox" />
-                                                    <input type="text" value={filterTrash.item} className='inputRead' />
+                                                    <input type="checkbox" className="checkbox" checked={filterTrash.checked} onChange={ () => handleCheck(filterTrash.id) }/>
+                                                    <input type="text" value={filterTrash.item} className={`inputRead ${filterTrash.checked && 'textThrought'}`} />
                                                 </div>
                                             </div>                                
                                         </div>
@@ -150,11 +170,11 @@ const Pager = () => {
                         }
                         
                         <div className="trashMenu">
-                            <button className="delete">
+                            <button className="delete" onClick={deleteForever}>
                                 <img src={Trash} alt="" />
                                 <p>Delete Forever</p>
                             </button>
-                            <button className="moveBack">
+                            <button className="moveBack" onClick={moveBackToToDo}>
                                 <img src={Library} alt="" />
                                 <p>Move Back To To Do</p>
                             </button>
@@ -177,10 +197,39 @@ const Pager = () => {
         setToDoText(e.target.value)
     }
 
-    function add() {
-        localStorage.setItem('inputTextArea', toDoText)
+    const add = () => {
+        const newItem = { id: Date.now(), checked: false, item: toDoText, onlyTrash: false, toDoDone: true }
+        setItems([...items, newItem]);
+        setToDoText('');
     }
 
+    const handleCheck = (id) => {
+        console.log(`key ${ id }`)
+        const listItems = items.map(item => item.id === id ? { ...item, checked: !item.checked } : item)
+        setItems(listItems)
+    }
+
+    const moveToTrash = () => {
+        const checkedItems = items.filter(item => item.checked);
+    
+        const updatedItems = items.map(item =>
+            checkedItems.find(checkedItem => checkedItem.id === item.id) ? { ...item, onlyTrash: true, toDoDone: false, checked: false } : item
+        );
+        setItems(updatedItems);
+    }
+
+    const moveBackToToDo = () => {
+        const checkedItems = items.filter(item => item.checked);
+        const updatedItems = items.map(item =>
+            checkedItems.find(checkedItem => checkedItem.id === item.id) ? {...item, checked: false, onlyTrash: false, toDoDone: true} : item
+        )
+        setItems(updatedItems);
+    }
+
+    const deleteForever = () => {
+        const checkedItems = items.filter(item => !item.checked);
+        setItems(checkedItems)
+    }
 
     return (
         <section className="pager">
@@ -218,7 +267,7 @@ const Pager = () => {
                 <div className="inputTask">
                     <div className="inputTaskWrapper">
                         <h1>Add New To Do</h1>
-                        <textarea type="text" className='tasks' placeholder="Your text" value={toDoText} />
+                        <textarea type="text" className='tasks' placeholder="Your text" value={toDoText} onChange={handleTextChange}/>
                         <button className="add" onClick={add}>
                             <span>Add</span>
                         </button>
